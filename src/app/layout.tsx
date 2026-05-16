4761fa9a015269dee2e86d1fa1253a3ea488e4f7
Galaxy IT <galaxyinfomkt@gmail.com>
2026-05-16 08:20:25 -0300

    Update chat widget id to 6a08522f0d6444ff0b67b12b

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -464,7 +464,7 @@ export default function RootLayout({
         <Script
           src="https://widgets.leadconnectorhq.com/loader.js"
           data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
-          data-widget-id="6a0843b50d6444370d649955"
+          data-widget-id="6a08522f0d6444ff0b67b12b"
           strategy="lazyOnload"
         />
       </body>
